0982e1e4d45436eeff202518fc96a5546738fc0a
dharmeshchauhan125782 <dharmeshchauhan9058@gmail.com>
2026-07-14 08:29:37 +0530

    Add files via upload

---
+++ P1.php
@@ -0,0 +1,12 @@
+<?php
+define("COLLEGE_NAME", "PrimeTech College");
+$studentName = "Vaibhav";
+$semester = "Semester 5";
+$percentage = 78.50;
+
+echo "Previous Semester Result<br>  ";
+echo "College Name: " . COLLEGE_NAME . "<br>";
+echo "Student Name: " . $studentName . "<br>";
+echo "Semester: " . $semester . "<br>";
+echo "Percentage: " . $percentage . "%";
+?>
