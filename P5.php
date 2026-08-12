d270ae842bd11e1caccae6446ab80fbcc709a595
dharmeshchauhan125782 <dharmeshchauhan9058@gmail.com>
2026-08-12 19:18:31 +0530

    Add files via upload

--- P5.php
+++ P5.php
@@ -1,17 +1,17 @@
 <?php
 echo "<h3>Using For Loop</h3>";
 
-for ($i = 5; $i <= 10; $i++) {
-    echo $i . " ";
+for($i=5;$i<=10;$i++)
+{
+    echo $i . "<br>";
 }
-
-echo "<br><br>";
 
 echo "<h3>Using Foreach Loop</h3>";
 
-$numbers = array(5, 6, 7, 8, 9, 10);
+$arr = array(5,6,7,8,9,10);
 
-foreach ($numbers as $num) {
-    echo $num . " ";
+foreach($arr as $value)
+{
+    echo $value . "<br>";
 }
 ?>
