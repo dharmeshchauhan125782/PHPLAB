c351a7fe59fbee2b34a30f2c5f26de337173554c
dharmeshchauhan125782 <dharmeshchauhan9058@gmail.com>
2026-08-12 19:15:53 +0530

    Add files via upload

--- P3.php
+++ P3.php
@@ -1,8 +1,10 @@
 <?php
-$a = 20;
-$b = 10;
+$a = 10;
+$b = 5;
+
 echo "Addition = " . ($a + $b) . "<br>";
 echo "Subtraction = " . ($a - $b) . "<br>";
 echo "Multiplication = " . ($a * $b) . "<br>";
 echo "Division = " . ($a / $b) . "<br>";
-
+echo "Modulus = " . ($a % $b) . "<br>";
+?>
