e2ecfc5937ff757df1b17745119f8761d2b9f29c
dharmeshchauhan125782 <dharmeshchauhan9058@gmail.com>
2026-08-15 21:03:21 +0530

    Add files via upload

---
+++ 3.1_create_cookie.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form method="post">
+    Enter Name:
+    <input type="text" name="name" required>
+    <input type="submit" name="submit" value="Save">
+</form>
+
+<?php
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    setcookie("username", $name, time() + 3600, "/");
+    echo "Cookie Created Successfully. Refresh the page to read it.";
+}
+?>
+</body>
+</html>
